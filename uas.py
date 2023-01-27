6878ab780a62646e81ec6b28a9f6a2d4708adce3
harisfahri <123728533+harisfahri@users.noreply.github.com>
2023-01-27 17:16:38 +0700

    Add files via upload

--- uas.py
+++ uas.py
@@ -8,29 +8,29 @@ if playing.lower() != "yes":
 print("Okay! Let's play :)")
 score = 0
 
-answer = input("What does CPU stand for? ")
-if answer.lower() == "central processing unit":
+question = input("What does CPU stand for? ")
+if question.lower() == "central processing unit":
     print('Correct!')
     score += 1
 else:
     print("Incorrect!")
 
-answer = input("What does GPU stand for? ")
-if answer.lower() == "graphics processing unit":
+question = input("What does GPU stand for? ")
+if question.lower() == "graphics processing unit":
     print('Correct!')
     score += 1
 else:
     print("Incorrect!")
 
-answer = input("What does RAM stand for? ")
-if answer.lower() == "random access memory":
+question = input("What does RAM stand for? ")
+if question.lower() == "random access memory":
     print('Correct!')
     score += 1
 else:
     print("Incorrect!")
 
-answer = input("What does PSU stand for? ")
-if answer.lower() == "power supply unit":
+question = input("What does PSU stand for? ")
+if question.lower() == "power supply unit":
     print('Correct!')
     score += 1
 else:
